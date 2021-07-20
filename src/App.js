f8ef1cd9cfc62125aaa7c85e4716db113025398b
Adriano <adriano.sotos@linx3.com>
2021-07-20 11:00:08 -0300

    feat: added provider and app routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/home/Home'
+import Details from './pages/details/Details'
+import Search from './pages/search/Search'
+import ReadBookProvider from './context/readBook'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReadBookProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+            <Home/>
+          </Route>
+          <Route path='/book/:bookId'>
+            <Details/>
+          </Route>
+          <Route path='/search'>
+            <Search/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </ReadBookProvider>
   );
 }
 
