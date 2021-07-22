14664e8cfe1afc0e0142be50ef789a5f738bc0fb
Adriano <adriano.sotos@linx3.com>
2021-07-22 19:44:45 -0300

    feat: save book feature

--- src/context/readBook.js
+++ src/context/readBook.js
@@ -1,9 +1,15 @@
-import React, { createContext, useState, useContext } from 'react'
+import React, { createContext, useState, useContext, useEffect } from 'react'
 
 const ReadBookContext = createContext()
 
 export default function ReadProvider ({ children }) {
-  const [readBook, setReadBook] = useState(null)
+  const [readBook, setReadBook] = useState([])
+
+  useEffect(() => {
+    const localStorageItem = JSON.parse(localStorage.getItem('read-book'))
+    setReadBook(localStorageItem)
+  }, [])
+
   return (
     <ReadBookContext.Provider value={{ readBook, setReadBook }}>
       {children}
@@ -15,5 +21,10 @@ export function useReadBook () {
   const context = useContext(ReadBookContext)
   const { readBook, setReadBook } = context
 
-  return { readBook, setReadBook }
+  function saveReadBook (readBook) {
+    setReadBook(readBook)
+    localStorage.setItem('read-book', JSON.stringify(readBook))
+  }
+
+  return { readBook, setReadBook, saveReadBook }
 }
