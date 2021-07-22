1429ea05aeb63053925f5f4512be1294a38c8280
Adriano <adriano.sotos@linx3.com>
2021-07-22 19:43:42 -0300

    feat: global styles

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+ 
+export const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background: teal;
+    font-family: Open-Sans, Helvetica, Sans-Serif;
+    background-color:  #F2F2F2;
+  }
+
+  html, body {
+    height: 100%;
+  }
+`;
